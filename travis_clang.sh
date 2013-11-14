5ecff0477eb2c6265bd701f92e4b447241f84b3d
Daniel Kirchner <daniel.c.kirchner@googlemail.com>
2013-11-15 00:07:00 +0100

    .

--- travis_clang.sh
+++ travis_clang.sh
@@ -4,6 +4,7 @@
 
 sudo apt-get --force-yes purge clang
 sudo add-apt-repository --yes 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise main'
+sudo wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key --yes add -
 sudo apt-get update
 sudo apt-get --force-yes install clang-3.4 clang-3.4-doc libclang-common-3.4-dev libclang-3.4-dev libclang1-3.4 libclang1-3.4-dbg libllvm-3.4-ocaml-dev libllvm3.4 libllvm3.4-dbg lldb-3.4 llvm-3.4 llvm-3.4-dev llvm-3.4-doc llvm-3.4-examples llvm-3.4-runtime cpp11-migrate-3.4 clang-format-3.4
 clang -v
