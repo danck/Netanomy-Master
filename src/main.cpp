9476c53a24053001c04e3960e21edc380e83960f
Daniel Kirchner <daniel.c.kirchner@googlemail.com>
2014-01-03 21:38:34 +0100

    GUI-Connector draft added

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,18 @@
 #include "config.hpp"
 #include "zhelpers.hpp"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
 using namespace nty;
 
 /**
-   Publishes a message on the socket.
+   Publishes a message on the publisher socket.
 
    @param[in]     _inArg1 Pointer to zmq context.
    @param[in]     _inArg2 Smart-pointer to logger.
@@ -62,8 +70,12 @@ collector(zmq::context_t* context, std::shared_ptr<Logger> logger)
         // collector.recv(&request);
         logger->log(result);
         zhelpers::s_send(collector, "ACK");
+
+        // TODO send result to GUI-Socket
     }
 }
+
+#define SOCK_PATH "/tmp/test.sock"
 
 /**
    Starts the master server.
@@ -81,7 +93,77 @@ main(int argc, char** argv)
     std::thread pub (publisher, &context, logger);
     std::thread col (collector, &context, logger);
 
-    // interacts with GUI and PUB+COLL
+    // TODO interacts with GUI and PUB+COLL
+    // * Server Socket aufsetzen
+    // * EINE Clientverbindung akzeptieren
+    // * listens for commands on ze GUI socket
+
+    int s, s2, len;
+    unsigned t;
+    struct sockaddr_un local, remote;
+    char str[100];
+
+    if ((s = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    local.sun_family = AF_UNIX;
+    strcpy(local.sun_path, SOCK_PATH);
+    unlink(local.sun_path);
+    len = strlen(local.sun_path) + sizeof(local.sun_family);
+    if (bind(s, (struct sockaddr *)&local, len) == -1) {
+        perror("bind");
+        exit(1);
+    }
+
+    if (listen(s, 5) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    for(;;) {
+        int done, n;
+        printf("Waiting for a connection...\n");
+        t = sizeof(remote);
+        if ((s2 = accept(s, (struct sockaddr *)&remote, &t)) == -1) {
+            perror("accept");
+            exit(1);
+        }
+
+        printf("Connected.\n");
+
+        done = 0;
+        do {
+          n = recv(s2, str, 100, 0);
+
+          if (n <= 0) {
+              if (n < 0) perror("recv");
+              done = 1;
+          }
+          if (!done) {
+            str[n] = '\0';
+            std::string msg(str);
+
+            unsigned delm = msg.find(" ");
+
+            std::string plugin_name = msg.substr (0, delm);
+            std::string domain_name = msg.substr (delm+1, msg.size() - delm - 2 );
+
+            logger->log(plugin_name);
+            logger->log(domain_name);
+            logger->log(msg);
+            // printf("%s %d modified\n",str, n); 
+            if (send(s2, str, n, 0) < 0) {
+                perror("send");
+                done = 1;
+            }
+          }
+        } while (!done);
+
+        close(s2);
+    }
+
 
     logger->log("Main: joining publisher and collector");
     pub.join();
